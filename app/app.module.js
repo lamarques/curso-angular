cb2492f46a7beac6d298cf33d3924953ccb822f4
Rogerio Lamarques <rogerio.lamarques@gmail.com>
2017-03-12 18:50:07 -0300

    Curso inicial de angular completo

--- app/app.module.js
+++ app/app.module.js
@@ -9,6 +9,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var app_component_1 = require("./app.component");
+var forms_1 = require("@angular/forms");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -16,8 +17,8 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule],
-        declarations: [app_component_1.AppComponent],
+        imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
+        declarations: [app_component_1.AppComponent, app_component_1.TaskEditComponent],
         bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
